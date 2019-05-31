dfae0030949ec0ba20dbb5a273cbf8c6819ef01a
Shai Reznik <shairez@users.noreply.github.com>
2019-05-31 12:34:52 +0300

    feat(async-spyable): deprecated @AsyncSpyable
    you don't need to use the @AsyncSpyable decorator anymore, it will get removed in the next major
    version

--- src/async-spyable-decorator.ts
+++ src/async-spyable-decorator.ts
@@ -1,5 +1,9 @@
 export function AsyncSpyable() {
-  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  // tslint:disable-next-line:no-console
+  console.warn(`The "AsyncSpyable" is depcracated,
+  You don\'t need to use it anymore.
 
-  };
+  IT WILL GET REMOVED IN THE NEXT MAJOR VERSION
+  So please remove it from your code.`);
+  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {};
 }
