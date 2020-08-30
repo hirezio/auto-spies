cddfce4ab0b11172ea6dc4523e63124982c025b6
Shai Reznik <shairez@users.noreply.github.com>
2020-08-31 01:14:37 +0300

    refactor(functionspy): rename createSpyFunction to createFunctionSpy
    BREAKING CHANGE: createSpyFunction is no longer available

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from './spy.types';
 export { createSpyFromClass } from './create-spy-from-class';
-export { createFunctionSpy as createSpyFunction } from './create-function-spy';
+export { createFunctionSpy } from './create-function-spy';
 export { recordObservable } from './observables/record-observable';
 export { ObserverSpy } from './observables/observer-spy';
