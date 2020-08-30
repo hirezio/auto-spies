3b0554711851a51a997e64b4fcf718acfef1fa2f
Shai Reznik <shairez@users.noreply.github.com>
2020-08-31 01:07:45 +0300

    chore(asyncspyable): remove @AsyncSpyable decorator
    it was depracated for a while, and isn't needed anymore
    
    BREAKING CHANGE: @AsyncSpyable is removed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './spy.types';
-export { AsyncSpyable } from './async-spyable-decorator';
 export { createSpyFromClass } from './create-spy-from-class';
 export { createFunctionSpy as createSpyFunction } from './create-function-spy';
 export { recordObservable } from './observables/record-observable';
