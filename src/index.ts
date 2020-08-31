2c142834d89af81bae9c3e80ce934830e657d250
Shai Reznik <shairez@users.noreply.github.com>
2020-08-31 10:10:55 +0300

    chore(observer-spy): remove observer-spy
    observer-spy has moved to @hirez_io/observer-spy
    
    BREAKING CHANGE: remove observerSpy and recordObservable from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export * from './auto-spies.types';
 export { createSpyFromClass } from './create-spy-from-class';
 export { createFunctionSpy } from './create-function-spy';
-export { recordObservable } from './observables/record-observable';
-export { ObserverSpy } from './observables/observer-spy';
