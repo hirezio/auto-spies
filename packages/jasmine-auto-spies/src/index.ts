00375924bca97ae6493c3e0beded703ed3493a8b
mopinsk <29751071+mopinsk@users.noreply.github.com>
2023-03-22 11:59:12 +0100

    refactor(packages/jasmine-auto-spies/src/create-spy-from-class.ts): move export to index.ts
    move re-export of ClassSpyConfiguration to index.ts (because Karma/Istanbul wrongly says it isn't
    covered by a test)

--- packages/jasmine-auto-spies/src/index.ts
+++ packages/jasmine-auto-spies/src/index.ts
@@ -1,5 +1,6 @@
 export * from './jasmine-auto-spies.types';
-export { createSpyFromClass, ClassSpyConfiguration } from './create-spy-from-class';
+export { ClassSpyConfiguration } from '@hirez_io/auto-spies-core';
+export { createSpyFromClass } from './create-spy-from-class';
 export { createFunctionSpy } from './create-function-spy';
 export { createObservableWithValues } from '@hirez_io/auto-spies-core';
 export * from './angular-provider-helper';
